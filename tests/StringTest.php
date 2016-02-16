d251401b76ce122d34e6938696d92a7e9794ce00
Václav Uruba <uruba@outlook.com>
2016-02-16 17:40:04 +0100

    – added tests for the Strings class

---
+++ tests/StringTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use FinanCalc\Utils\Strings;
+
+/**
+ * Class StringTest
+ */
+class StringTest extends PHPUnit_Framework_TestCase
+{
+    public function testNonExistentLocale() {
+        $this->assertNull(Strings::getString('message_incompatible_types', 'non_existent_locale'));
+    }
+
+    public function testNonExistentIdentifier() {
+        $this->assertNull(Strings::getString('non_existent_identifier'));
+        $this->assertNull(Strings::getFormattedString('non_existent_identifier', null));
+    }
+
+    public function testGetString() {
+        $this->assertEquals(
+            'The value has to be of the type %s, but currently is of the type %s instead.',
+            Strings::getString('message_incompatible_types')
+        );
+
+        $this->assertEquals(
+            'The value has to be of the type goodType, but currently is of the type badType instead.',
+            Strings::getFormattedString('message_incompatible_types', null, 'goodType', 'badType')
+        );
+    }
+}
